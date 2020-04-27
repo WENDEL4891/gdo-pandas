5bfd1698d1d2798056aa0c42c828a18c5bd3420a
wendel4891 <wendel4891@gmail.com>
2020-04-27 01:06:44 -0300

    avanços no notebook importação de novos arquivos.

--- funcs.py
+++ funcs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def get_files_names(only_not_imported=False):
@@ -46,3 +47,139 @@ def get_files_names(only_not_imported=False):
         'rate':rats_efetivo_files,
         'ratp':rats_produtividade_files
     }
+
+
+def get_df_classif():
+    df_classif = pd.read_csv('files/classificadores.csv')    
+    df_classif.set_index( df_classif['MUNICIPIO'] + " " + df_classif['VALIDADOR_TIPO'] + " " + df_classif['VALIDADOR'], inplace=True)    
+    df_classif.fillna('', inplace=True)
+    df_classif = df_classif.reset_index().drop_duplicates('index').set_index('index')
+    return df_classif
+
+def filter_23(df):
+    return df[
+        df['MUNICIPIO'].isin([
+            'DIVINOPOLIS',
+            'ITAUNA',
+            'ITATIAIUCU',
+            'CARMO DO CAJURU',
+            'SAO GONCALO DO PARA',
+            'CLAUDIO'
+        ])
+    ]
+
+
+def read_files(files_path_names, df_rat=None, rat_data=True):
+    for i in range( len(files_path_names) ):    
+        if i == 0:        
+            df = pd.read_csv('files/RAT/' + files_path_names[i], error_bad_lines=False, sep='|')
+            df = df.applymap(lambda x: x.strip() if type(x) == str else x)    
+            if rat_data:
+                df = filter_23(df)
+            else:
+                df = df[
+                    df.iloc[:,0].isin(df_rat.index)
+                ]
+        else:
+            df_aux = pd.read_csv('files/RAT/' + files_path_names[i], error_bad_lines=False, sep='|')
+            df_aux = df_aux.applymap(lambda x: x.strip() if type(x) == str else x)
+            if rat_data:
+                df = filter_23(df)
+            else:
+                df = df[
+                    df.iloc[:,0].isin(df_rat.index)
+                ]
+            df = pd.concat([df, df_aux])
+    return df
+
+
+def data_rat_processing(df_rat):
+    df_rat.drop_duplicates(subset='RAT.NUM_ATIVIDADE', keep='last', inplace=True)
+
+    s_dta_in = df_rat['DTA_INICIO'] + " " + df_rat['HRA_INICIO']
+    df_rat.loc[:,'DTA_HRA_INICIO_DT'] = pd.to_datetime( s_dta_in, format='%d/%m/%Y %H:%M', errors='coerce')
+    del s_dta_in
+
+    s_dta_ter = df_rat.loc[:,'DTA_TERMINO'] + " " + df_rat.loc[:,'HRA_TERMINO']
+    df_rat.loc[:,'DTA_HRA_TERMINO_DT'] = pd.to_datetime( s_dta_ter, format='%d/%m/%Y %H:%M', errors='coerce')
+    del s_dta_ter
+
+    df_rat.loc[:,'TEMPO_DT'] = df_rat['DTA_HRA_TERMINO_DT'] - df_rat['DTA_HRA_INICIO_DT']
+
+    df_rat.loc[:,'TEMPO_INT'] = df_rat['TEMPO_DT'].dt.total_seconds() / 60
+
+    df_rat.loc[:,'DIA'] = df_rat.loc[:,'DTA_HRA_INICIO_DT'].dt.day
+    
+    df_rat.loc[:,'MES'] = df_rat.loc[:,'DTA_HRA_INICIO_DT'].dt.month
+
+    df_rat.loc[:,'ANO'] = df_rat.loc[:,'DTA_HRA_INICIO_DT'].dt.year
+
+    df_rat.loc[:,'DEZENA'] = np.select([
+        df_rat['DTA_HRA_INICIO_DT'].dt.day <= 10,
+        df_rat['DTA_HRA_INICIO_DT'].dt.day <= 20
+    ],[1,2], default=3)
+
+
+    for field in ['TEMPO_INT', 'DIA', 'MES', 'ANO', 'DEZENA']:
+        df_rat[field].fillna(0, inplace=True)
+        df_rat[field] = df_rat[field].astype('int32')
+
+    cols_classif = [
+        'MUNICIPIO',
+        'LOGRADOURO',
+        'DES_ENDERECO',
+        'COMPLEMENTO_ENDERECO',
+        'NOME_BAIRRO',
+        'LOGRADOURO2',
+        'DES_ENDERECO2'
+    ]
+    df_rat[cols_classif] = df_rat[cols_classif].apply(lambda col: col.str.upper())
+    df_rat[cols_classif] = df_rat[cols_classif].fillna('')
+
+    return df_rat
+
+
+df_classif = get_df_classif()
+def classifica_setor(row):
+    mun = row['MUNICIPIO']
+    if mun == 'CLAUDIO':        
+        return 'CLAUDIO'
+    elif mun == 'ITATIAIUCU':
+        return 'LOURDES/ITATIAIUCU'
+    elif mun in ('CARMO DO CAJURU', 'SAO GONCALO DO PARA'):             
+        return 'CARMO DO CAJURU/SAO GONCALO DO PARA'    
+    elif ( mun + " N_RAT " + row['RAT.NUM_ATIVIDADE'] ) in df_classif.index:
+        return df_classif.loc[mun+" N_RAT "+row['RAT.NUM_ATIVIDADE'], 'SETOR']
+    elif mun + ' BAIRRO ' + row['NOME_BAIRRO'] in df_classif.index:       
+        return ( df_classif.loc[mun + ' BAIRRO ' + row['NOME_BAIRRO'].upper(), 'SETOR'] ).upper()
+    elif mun + ' LOGRADOURO ' + row['LOGRADOURO'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO ' + row['LOGRADOURO'].upper(), 'SETOR']
+    elif mun + ' LOGRADOURO ' + row['DES_ENDERECO'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO ' + row['DES_ENDERECO'].upper(), 'SETOR']
+    elif mun + ' LOGRADOURO_NAO_CAD ' + row['LOGRADOURO2'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO_NAO_CAD ' + row['LOGRADOURO2'].upper(), 'SETOR']
+    elif mun + ' LOGRADOURO_NAO_CAD ' + row['DES_ENDERECO'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO_NAO_CAD ' + row['DES_ENDERECO'].upper(), 'SETOR']
+    elif mun + ' LOGRADOURO_NAO_CAD ' + row['LOGRADOURO2'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO_NAO_CAD ' + row['LOGRADOURO2'].upper(), 'SETOR']
+    elif mun + ' LOGRADOURO_NAO_CAD ' + row['DES_ENDERECO2'] in df_classif.index:
+        return df_classif.loc[mun + ' LOGRADOURO_NAO_CAD ' + row['DES_ENDERECO2'].upper(), 'SETOR']
+    elif mun + ' COMPLEMENTO_END ' + row['COMPLEMENTO_ENDERECO'] in df_classif.index:
+        return df_classif.loc[mun + ' COMPLEMENTO_END ' + row['COMPLEMENTO_ENDERECO'].upper(), 'SETOR']
+    elif ( mun + ' COMPLEMENTO_END ' + row['DES_ENDERECO'] ) in df_classif.index:
+        return df_classif.loc[mun + ' COMPLEMENTO_END ' + row['DES_ENDERECO'], 'SETOR']    
+    else:
+        return 'other'
+
+
+def classifica_cia(df_rat):
+    conds=[
+        df_rat['MUNICIPIO'].isin(['ITAUNA','ITATIAIUCU']),
+        df_rat['SETOR'].isin(['HIPER CENTRO','BOM PASTOR','ALTO GOIAS']),
+        df_rat['SETOR'].isin(['PLANALTO','SAO JOSE','CLAUDIO']),
+        df_rat['SETOR'].isin(['NITEROI','PORTO VELHO','CARMO DO CAJURU/SAO GONCALO DO PARA']),
+        
+    ]
+    res=['51 CIA','53 CIA','139 CIA','142 CIA']
+    df_rat['CIA'] = np.select(conds,res,default='other')
+
